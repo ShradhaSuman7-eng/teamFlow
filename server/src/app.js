1005644160086dfd1202a1a948a700b3df05bd19
Suman Shradha <suman.shradha.36.4@mountblue.tech>
2026-08-30 23:39:04 +0530

    feat: add health check endpoint

--- server/src/app.js
+++ server/src/app.js
@@ -6,4 +6,11 @@ app.get("/", (req, res) => {
   res.send("TeamFLow API is Running");
 });
 
+app.get("/api/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "TeamFlow API is healthy",
+  });
+});
+
 export default app;
