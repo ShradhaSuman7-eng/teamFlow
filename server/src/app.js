75cab6b03cc27c10a1fe455c525fd742f856de8e
Suman Shradha <suman.shradha.36.4@mountblue.tech>
2026-08-31 10:03:46 +0530

    feat: add centralized error handling

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import healthRoute from "./routes/health.routes.js";
+import logger from "./middleware/logger.middleware.js";
+import errorHandler from "./middleware/error.middleware.js";
 
 const app = express();
+
+app.use(logger);
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("TeamFlow API is Running");
 });
 
 app.use("/api", healthRoute);
+app.use("/api/test", testRouter);
+app.use(errorHandler);
 
 export default app;
